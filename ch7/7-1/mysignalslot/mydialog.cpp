3c7b46dd014ad5a73b8dd7d9d5ba2222f81a3ce0
1802802 <1044889045@qq.com>
2019-02-20 21:15:49 +0800

    2019_2_20 7-1 信号与槽1

---
+++ ch7/7-1/mysignalslot/mydialog.cpp
@@ -0,0 +1,23 @@
+﻿#include "mydialog.h"
+#include "ui_mydialog.h"
+#include <QDebug>
+
+MyDialog::MyDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::MyDialog)
+{
+    ui->setupUi(this);
+}
+
+MyDialog::~MyDialog()
+{
+    delete ui;
+}
+
+void MyDialog::on_pushButton_clicked()  //这种形式的是从UI设计师界面中自动生成的信号与槽关联
+{
+    int value = ui->spinBox->value();  //获取输入的值
+    emit dlgReturn(value);             //发射信号,emit为发射函数
+    qDebug()<<QString::fromLocal8Bit("MyDialog对话框中的信号发射");
+    close();
+}
---
+++ ch7/7-1/mysignalslot/widget.cpp
@@ -0,0 +1,27 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include "mydialog.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    MyDialog *dlg = new MyDialog(this);
+    //connect(dlg,SIGNAL(dlgReturn(int)),this,SLOT(showValue(int)));  //关联对话框的数值返回信号与窗口的显示数的槽
+    connect(dlg,&MyDialog::dlgReturn,this,&Widget::showValue);  //关联的另一种重载形式
+    //connect(dlg,SIGNAL(dlgReturn(int)),this,SLOT(showValue(int)),Qt::QueuedConnection);   //最后的type为连接关系
+    dlg->show();
+    dlg->exec(); //用于实现模态对话框
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::showValue(int Value)
+{
+    ui->label->setText(QString::fromLocal8Bit("显示的数值为：%1").arg(Value));
+}
