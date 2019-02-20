cd0efdde5ff7c145e0c4f59a0d09dac7a4181985
1802802 <1044889045@qq.com>
2019-02-20 21:01:23 +0800

    2019_2_20 5-1-5 主界面栏操作1

---
+++ ch5/5-1-5/myaction/mainwindow.h
@@ -0,0 +1,26 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void setText(const QString &string);
+};
+
+#endif // MAINWINDOW_H
