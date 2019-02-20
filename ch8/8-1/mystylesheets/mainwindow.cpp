2121933dd315a65c7ff33abb02398848530a9f0a
1802802 <1044889045@qq.com>
2019-02-20 20:26:33 +0800

    2019_2_20

---
+++ ch8/8-1/mystylesheets/mainwindow.cpp
@@ -0,0 +1,22 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //单独对部件设置样式表（注意样式表stylesheets和风格style不是一种东西）
+//    ui->pushButton->setStyleSheet("background:yellow");
+//    ui->horizontalSlider->setStyleSheet("background:red");
+
+    //对所有的PushButton和QSlide部件进行样式表设置
+//    setStyleSheet("QPushButton{background:yellow}QSlider{background:red}");
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
