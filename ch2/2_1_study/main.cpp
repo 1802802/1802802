acaf00350e74bf9090caf1afade7ce7c35d59979
1802802 <1044889045@qq.com>
2019-02-20 20:54:45 +0800

    2019_2_20 2-1基本对话框

---
+++ ch2/2_1_study/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "hellodialog.h"
+#include <QApplication>
+
+//2019_2_20 上传
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    HelloDialog w;
+    w.show();
+
+    return a.exec();
+}
