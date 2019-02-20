230f02194e9e9571a23f696ba68fd0d71f82a363
1802802 <1044889045@qq.com>
2019-02-20 21:33:54 +0800

    2019_2_20 8-1 Qt风格

---
+++ ch8/8-1/mystyle/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    a.setStyle(QStyleFactory::create("fuison"));  //整体风格
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ ch8/8-1/mystyle/mainwindow.cpp
@@ -0,0 +1,28 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QStyleFactory>
+#include <QPalette>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    ui->progressBar->setStyle(QStyleFactory::create("windows"));  //单独部件的风格
+
+    QPalette palette = ui->pushButton->palette();
+    palette.setColor(QPalette::Button,Qt::red);
+    palette.setColor(QPalette::Base,Qt::yellow);
+    palette.setColor(QPalette::ButtonText,Qt::yellow);
+    ui->pushButton->setPalette(palette);
+    ui->spinBox->setDisabled(true);
+    QPalette palette2 = ui->spinBox->palette();
+    palette2.setColor(QPalette::Disabled,QPalette::Base,Qt::green);
+    ui->spinBox->setPalette(palette2);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
