f99fda25ad356324eb432e7a896295966f49b32b
1802802 <1044889045@qq.com>
2019-02-20 21:09:42 +0800

    2019_2_20 6-2 鼠标事件与滚轮事件

---
+++ ch6/6-2/mymouseevent/widget.cpp
@@ -0,0 +1,77 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QMouseEvent>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    QCursor cursor;                             //创建一个新的光标对象
+    cursor.setShape(Qt::OpenHandCursor);        //对新光标设置形状（张开的手）
+    setCursor(cursor);                          //将新光标设置为当前使用的光标
+
+    setMouseTracking(true);    //开启窗口模式（Widget）的鼠标跟踪
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)  //左右键按下后切换光标
+{
+    if(event->button() == Qt::LeftButton)         //如果传递的鼠标事件是鼠标左键按下事件
+    {
+        QCursor cursor;
+        cursor.setShape(Qt::ClosedHandCursor);
+        QApplication::setOverrideCursor(cursor);  //将鼠标指针暂时变为闭合的手的形状
+        offset = event->globalPos() - pos();      //获取指针的位置和窗口位置的差值
+        qDebug()<<tr("offset x:%1 y:%2").arg(offset.x()).arg(offset.y());
+    }
+    else if(event->button() == Qt::RightButton)   //如果传递的鼠标事件是鼠标右键按下事件
+    {
+        QCursor cursor(QPixmap("D:/Users/CAD/Documents/QT_Study/ch6/6-2/mymouseevent/logo.png"));
+        QApplication::setOverrideCursor(cursor);  //将自定义的图片作为鼠标指针
+    }
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)   //按住左键后可以拖动窗口移动
+{
+    if(event->buttons() & Qt::LeftButton)  //鼠标移动时所有的键都会被检测，所以判断buttons和左键的“与”是否为真
+    {
+        QPoint temp;
+        temp = event->globalPos() - offset;//此数据是随着鼠标的移动而不断实时计算的
+        qDebug()<<tr("temp x:%1 y:%2").arg(temp.x()).arg(temp.y());
+        move(temp);                  //使用鼠标指针当前的位置减去差值，就得到了窗口应该移动到的位置
+    }
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *event)  //释放鼠标左键后，光标形状复原
+{
+    Q_UNUSED(event);            //明示不使用形参event，从而不产生警报
+    QApplication::restoreOverrideCursor();  //光标形状复原
+}
+
+void Widget::mouseDoubleClickEvent(QMouseEvent *event)  //双击鼠标左键全屏，右键提示
+{
+    if(event->button() == Qt::LeftButton)     //这是双击鼠标左键的操作
+    {
+        if(windowState()!= Qt::WindowFullScreen)   //如果不是全屏就全屏，是全屏就回到原大小
+            setWindowState(Qt::WindowFullScreen);
+        else
+            setWindowState(Qt::WindowNoState);
+    }
+    else if(event->button() == Qt::RightButton)
+        qDebug()<<QString::fromLocal8Bit("双击左键全屏");
+}
+
+void Widget::wheelEvent(QWheelEvent *event)   //滑动滚轮使文本编辑区的字放大/缩小
+{
+    if(event->delta()>0)          //向前滑和向后滑的区别就是这个delta的值
+        ui->textEdit->zoomIn();   //文本编辑区的字放大
+    else
+        ui->textEdit->zoomOut();  //文本编辑区的字缩小
+}
---
+++ ch6/6-2/mymouseevent/widget.h
@@ -0,0 +1,30 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+    QPoint offset;  //用于存储鼠标指针位置与窗口位置的差值
+
+protected:    //只要在头文件中声明了的函数，都必须要声明后才能运行。这里的函数都是已有的，此处只是对其进行重载，所以对应的操作—函数接口都是不需要改变的
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseDoubleClickEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+};
+
+#endif // WIDGET_H
