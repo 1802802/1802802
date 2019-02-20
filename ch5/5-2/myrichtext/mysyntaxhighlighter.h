f659e9f539a7c7f55f6b48deb16d8abd386d53ac
1802802 <1044889045@qq.com>
2019-02-20 21:03:59 +0800

    2019_2_20 5-2 富文本操作

---
+++ ch5/5-2/myrichtext/mysyntaxhighlighter.h
@@ -0,0 +1,16 @@
+﻿#ifndef MYSYNTAXHIGHLIGHTER_H
+#define MYSYNTAXHIGHLIGHTER_H
+
+#include <QSyntaxHighlighter>
+
+class MySyntaxHighlighter : public QSyntaxHighlighter
+{
+    Q_OBJECT      //在Qt中，如果一个类要使用信号/槽功能，就必须在其中声明Q_OBJECT
+public:
+    explicit MySyntaxHighlighter(QTextDocument *parent = 0);
+
+protected:
+    void highlightBlock(const QString &text); //必须重新实现该函数
+};
+
+#endif // MYSYNTAXHIGHLIGHTER_H
