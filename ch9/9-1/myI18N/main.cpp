55c918073ecc83bf0fca7be6123531c5882debd1
1802802 <1044889045@qq.com>
2019-02-21 16:35:42 +0800

    2019_2_21 9-1 Qt的国际化

---
+++ ch9/9-1/myI18N/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;   //翻译的代码务必放在MainWindow的创建之前
+    translator.load("D:/Users/CAD/Documents/QT_Study/ch9/9-1/myI18N/myI18N_zh_CN.qm");
+    a.installTranslator(&translator);
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
