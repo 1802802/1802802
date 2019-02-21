203825ee3954f491fe045aaea43aa35ddd5d7a7c
1802802 <1044889045@qq.com>
2019-02-21 17:07:40 +0800

    2019_2_21 9-2 帮助系统

---
+++ ch9/9-2/mywhatsthis/mainwindow.cpp
@@ -0,0 +1,18 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QWhatsThis>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QAction *action = QWhatsThis::createAction(this);
+    ui->mainToolBar->addAction(action);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
